83a5149e131718b40bcbf86bc30ebfbd94128ce3
Sefa Eren <sefa.eren000@gmail.com>
2022-11-15 18:56:20 +0300

    Login detailed

--- wb/views.py
+++ wb/views.py
@@ -3,7 +3,8 @@ import requests
 
 views = Blueprint('views', __name__)
 
-dc_auth = "https://discord.com/api/oauth2/authorize?client_id=1024735385865175180&redirect_uri=http%3A%2F%2Flocalhost%3A8000%2Foauth2%2Flogin%2Fredirect%2F&response_type=code&scope=identify"
+#dc_auth = "https://discord.com/api/oauth2/authorize?client_id=1024735385865175180&redirect_uri=http%3A%2F%2Flocalhost%3A8000%2Foauth2%2Flogin%2Fredirect%2F&response_type=code&scope=identify"
+dc_auth = "https://discord.com/api/oauth2/authorize?client_id=1024735385865175180&redirect_uri=http%3A%2F%2Flocalhost%3A8000%2Foauth2%2Flogin%2Fredirect%2F&response_type=code&scope=identify%20guilds"
 #code M5oroPyTLgWqQFCVXgKjxTX5EESdJI
 @views.route('/')
 def home():
@@ -11,7 +12,7 @@ def home():
 @views.route('/logout')
 def logout():
   session.clear()
-  flash("Çıkış yapıldı","error")
+  flash("Çıkış yapıldı","merror")
   return redirect(url_for('views.home'))
 @views.route('/commands')
 def commands():
@@ -25,15 +26,32 @@ def changelog():
 def oauth2_login_redirect():
     code = request.args.get('code')
     print(code)
-    user = exchange_code(code)
+    user,guilds = exchange_code(code)
     session['user'] = user
+    session["guilds"] = guilds
     return redirect(url_for('views.profile'))
   
 @views.route('/me')
 def profile():
-  if "user" in session:
+  # get user info from database - roles etc
+  # add column to database in user table for roles and sign in with discord code
+  if 'guilds' in session and "user" in session:
     user = session['user']
-    return render_template("profile.html",user=user)
+    user_info = {
+      "username": user['username'],
+      "fullname": f"{user['username']}#{user['discriminator']}",
+      "avatar_path": f"https://cdn.discordapp.com/avatars/{user['id']}/{user['avatar']}.png?size=1024",
+      "banner_color": user["banner_color"]
+      #banner-avatar_decoration
+    }
+    guild_session = session["guilds"]
+    isSign = True #database control for signup in the server
+    for servers in guild_session:
+      if servers["name"] == "BTÜ Bilgisayar Mühendisliği" and isSign:
+        return render_template("profile.html",user=user_info,guilds=session['guilds'])
+    else:
+      flash("Sunucuya giriş yapmadınız","m-error")
+      return redirect(url_for('views.home'))
   else:
     flash("You need to login first","error")
     return redirect(url_for('views.home'))
@@ -58,7 +76,12 @@ def exchange_code(code:str):
   response = requests.get("https://discord.com/api/v6/users/@me", headers={
     "Authorization": "Bearer %s" % access_token
     })
+  response1 = requests.get("https://discord.com/api/v6/users/@me/guilds", headers={
+    "Authorization": "Bearer %s" % access_token
+    })
+  print(f"{response1.json()}")
   print(response)
   user = response.json()
+  guilds = response1.json()
   print(user)
-  return user
+  return user, guilds
